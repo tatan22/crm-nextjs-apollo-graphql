e9e0e094f511669044c155f83f6e9b49fef8f851
tatancarduar <tatancarduar@hotmail.com>
2026-05-11 13:50:51 -0500

    feat(frontend): add products routing page

---
+++ FrontEnd/pages/nuevoproducto.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+const NuevoProducto = () => {
+	return (
+		<div>
+			<Layout>
+				<h1 className="text-2xl text-gray-800 font-light">Crear Nuevo Producto</h1>
+			</Layout>
+		</div>
+	)
+};
+
+export default NuevoProducto;
--- FrontEnd/pages/productos.js
+++ FrontEnd/pages/productos.js
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
 import { gql, useQuery } from "@apollo/client";
 import Producto from "../components/Producto";
+import Link from "next/link";
 
 const OBTENER_PRODUCTOS = gql`
 	query obtenerProductos {
@@ -13,30 +14,35 @@ const OBTENER_PRODUCTOS = gql`
 	}
 `;
 const Productos = () => {
-  // consultar los productos
-  const { data, loading, error } = useQuery(OBTENER_PRODUCTOS);
-  if (loading) return <p>Cargando...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+	// consultar los productos
+	const { data, loading, error } = useQuery(OBTENER_PRODUCTOS);
+	if (loading) return <p>Cargando...</p>;
+	if (error) return <p>Error: {error.message}</p>;
 	return (
 		<div className="">
 			<Layout>
 				<h1 className="text-2xl text-gray-800 font-light">Productos</h1>
-        <table className="table-auto shadow-md mt-10 w-full w-lg">
-				<thead className="bg-gray-800">
-					<tr className="text-white">
-						<th className="w-1/5 py-2">Nombre</th>
-						<th className="w-1/5 py-2">Precio</th>
-						<th className="w-1/5 py-2">Existencia</th>
-						<th className="w-1/5 py-2">Acciones</th>
-					</tr>
-				</thead>
-    
-				<tbody className="bg-white">
-					{data.obtenerProductos.map((producto) => (
-						<Producto key={producto.id} producto={producto} />
-					))}
-				</tbody>
-			</table>
+				<Link href="/nuevoproducto">
+					<a className="bg-blue-800 w-full sm:w-auto font-bold uppercase  text-sm text-white rounded py-2 px-5 shadow-md hover:bg-blue-700 hover:cursor-pointer mt-5 inline-block">
+						Nuevo Producto
+					</a>
+				</Link>
+				<table className="table-auto shadow-md mt-10 w-full w-lg">
+					<thead className="bg-gray-800">
+						<tr className="text-white">
+							<th className="w-1/5 py-2">Nombre</th>
+							<th className="w-1/5 py-2">Precio</th>
+							<th className="w-1/5 py-2">Existencia</th>
+							<th className="w-1/5 py-2">Acciones</th>
+						</tr>
+					</thead>
+
+					<tbody className="bg-white">
+						{data.obtenerProductos.map((producto) => (
+							<Producto key={producto.id} producto={producto} />
+						))}
+					</tbody>
+				</table>
 			</Layout>
 		</div>
 	);
