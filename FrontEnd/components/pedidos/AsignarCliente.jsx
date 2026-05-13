64b686ffd2638077673395b5decc8594d803a7cf
tatancarduar <tatancarduar@hotmail.com>
2026-05-13 09:17:40 -0500

    docs: consume state y actions desde useReducer

--- FrontEnd/components/pedidos/AsignarCliente.jsx
+++ FrontEnd/components/pedidos/AsignarCliente.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import Select from "react-select";
+// import Select from "react-select";
 
-// const options = [
-// 	{ value: "chocolate", label: "Chocolate" },
-// 	{ value: "strawberry", label: "Strawberry" },
-// 	{ value: "vanilla", label: "Vanilla" },
-// ];
+import dynamic from "next/dynamic";
+
+const Select = dynamic(() => import("react-select"), {
+  ssr: false,
+});
+
 const clientes = [
 	{ id: 1, nombre: "Jhonatan" },
 	{ id: 2, nombre: "Mariana" },
--- FrontEnd/pages/nuevopedido.jsx
+++ FrontEnd/pages/nuevopedido.jsx
@@ -1,25 +1,14 @@
-import { useState, useEffect } from "react";
+import { useContext } from "react";
 import Layout from "../components/Layout";
-import Select from "react-select";
 import AsignarCliente from "../components/pedidos/AsignarCliente";
-import { gql, useQuery } from "@apollo/client";
-
-const OBTENER_CLIENTES_USUARIOS = gql`
-  query obtenerClientesVendedor {
-    obtenerClientesVendedor {
-      id
-      nombre
-      apellido
-      empresa
-      email
-    }
-  }
-`;
+// Context de pedidos
+import PedidoContext from "../context/pedidos/PedidoContext";
 
 const NuevoPedido = () => {
-  const { data, loading } = useQuery(OBTENER_CLIENTES_USUARIOS);
-  if (loading) return "Cargando...";
-  if (!data.obtenerClientesVendedor) return null;
+	// Utilizar context y extraer sus funciones y valores
+	const pedidoContext = useContext(PedidoContext);
+	const { holaMundo } = pedidoContext;
+  holaMundo();
 	return (
 		<Layout>
 			<h1 className="text-2xl text-gray-800 font-light">Crear Nuevo Pedido</h1>
